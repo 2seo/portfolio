1e32959bdbb4b6ccb674f9cf4d1a473057fdeb1f
2seo <63112170+2seo@users.noreply.github.com>
2024-01-19 08:47:58 +0000

    🛠Feat: home div 타이핑 인터랙션 작성

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,15 @@ window.addEventListener('mousemove', (e) => {
 });
 
 const mouseMoveLoop = () => {
-  targetX += (x-targetX)*speed;
-  targetY += (y-targetY)*speed;
+  targetX += (x - targetX) * speed;
+  targetY += (y - targetY) * speed;
 
+  if (targetX - x > 10) {
+    circle.style.transform = "scaleX(-1)";
+  }
+  if (targetX - x < -10) {
+    circle.style.transform = "scaleX(1)";
+  }
   circle.style.top = targetY + "px";
   circle.style.left = targetX + "px";
   window.requestAnimationFrame(mouseMoveLoop);
@@ -26,9 +32,27 @@ const darkToggleHandler = () => {
   onOff = !onOff;
   if (onOff) {
     document.documentElement.setAttribute('color-theme', 'dark');
+    document.querySelector('.dicon').src = "assets/blackSun.svg";
   } else {
     document.documentElement.setAttribute('color-theme', 'light');
+    document.querySelector('.dicon').src = "assets/whiteMoon.svg";
   }
 }
 
 dark.addEventListener('click', darkToggleHandler);
+
+const text = "안녕하세요.\n프론트엔드 개발자를 꿈꾸는 송미숙입니다.";
+const tspan = document.querySelector('.typed');
+let i = 0;
+
+const typing = () => {
+  const t = text[i++];
+  if (i <= text.length) {
+    tspan.innerHTML += t === "\n" ? "<br/>" : t;
+  }
+  if (i > text.length + 10) {
+    tspan.textContent = "";
+    i = 0;
+  }
+}
+setInterval(typing, 200)
